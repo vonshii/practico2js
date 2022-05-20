9aff1afb74c9e18a1c92a77242bf30baa41ad6c6
vonshii <germanvscheidt@gmail.com>
2022-05-20 18:19:12 -0300

    primer commit, se agrego el index.html

---
+++ bucle1.js
@@ -0,0 +1,21 @@
+/* while
+while (condicion logica)
+{
+    todo el codigo q quiero repetir varias veces
+    cambiar la condicion logica
+
+}
+
+*/
+
+var edad = parseInt(prompt('ingrese su edad'));
+
+if (edad >= 18){
+    document.write('Ya tienes edad de conducir')
+}
+else
+{
+    document.write('El numero ingresado es incorrecto')
+}
+
+
