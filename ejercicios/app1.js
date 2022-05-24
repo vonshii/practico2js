c22837f06d35d5f9de3ee0918fd347744a831927
vonshii <germanvscheidt@gmail.com>
2022-05-23 23:39:40 -0300

    se agrego el ejercicio 1

---
+++ ejercicios/app1.js
@@ -0,0 +1,10 @@
+// ejercicio 1
+var edad = parseInt(prompt('ingrese su edad'));
+
+if (edad >= 18){
+    document.write('Ya tienes edad de conducir')
+}
+else
+{
+    document.write('El numero ingresado es incorrecto')
+}
